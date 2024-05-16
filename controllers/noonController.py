914458ddc87c5f611b814eac4066a6b746fd9c39
ahmedl3swy <ahmed.a.alesawy@gmail.com>
2024-05-16 23:16:20 +0300

    updated noonController

--- controllers/noonController.py
+++ controllers/noonController.py
@@ -7,20 +7,19 @@ async def scrape_noon_full(url: str) -> dict:
     start_time = time.time()
     asession = AsyncHTMLSession()
     response = await asession.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    title = soup.find('h1').get_text().strip()
-    price = soup.find('div', class_='priceNow').get_text().split('.')[0].strip()
-    rating = soup.find('div', class_='sc-e7071e85-2').get_text().strip()
-    description = soup.find('div', class_='sc-97eb4126-2').get_text().strip()
-    image_soup = soup.find_all('img')
-    img=[]
-    for i in image_soup:
-        if "&width" in i['src']:
-            img.append(i['src'])
-    end_time = time.time()
-    item_data = ItemData(title=title, price=price, rating=rating, description=description, images=img)
-    return item_data
+    # soup = BeautifulSoup(response.content, 'html.parser')
+    # title = soup.find('h1').get_text().strip()
+    # price = soup.find('div', class_='priceNow').get_text().split('.')[0].strip()
+    # rating = soup.find('div', class_='sc-e7071e85-2').get_text().strip()
+    # description = soup.find('div', class_='sc-97eb4126-2').get_text().strip()
+    # image_soup = soup.find_all('img')
+    # img=[]
+    # for i in image_soup:
+    #     if "&width" in i['src']:
+    #         img.append(i['src'])
+    # end_time = time.time()
+    # item_data = ItemData(title=title, price=price, rating=rating, description=description, images=img)
+    return {"response": response}
 
 async def scrape_noon_price(url: str) -> dict:
     start_time = time.time()
