87fd474a659446d962663b203acbb6e442e7b52e
ahmedl3swy <ahmed.a.alesawy@gmail.com>
2024-05-17 00:00:20 +0300

    added timeout error

--- controllers/noonController.py
+++ controllers/noonController.py
@@ -1,12 +1,18 @@
 import time
 from bs4 import BeautifulSoup
+import requests
 from requests_html import AsyncHTMLSession
 from models.Product import ItemData, ItemImages, ItemPrice
+from fake_headers import Headers
 
 async def scrape_noon_full(url: str) -> dict:
-    start_time = time.time()
+    headers = Headers(headers=True).generate()
     asession = AsyncHTMLSession()
-    response = await asession.get(url, timeout=10)
+    try:
+     response = await asession.get(url, headers=headers,timeout=10)
+    except:
+        return {"error": "timeout"}
+    
     soup = BeautifulSoup(response.content, 'html.parser')
     title = soup.find('h1').get_text().strip()
     price = soup.find('div', class_='priceNow').get_text().split('.')[0].strip()
@@ -17,27 +23,31 @@ async def scrape_noon_full(url: str) -> dict:
     for i in image_soup:
         if "&width" in i['src']:
             img.append(i['src'])
-    end_time = time.time()
     item_data = ItemData(title=title, price=price, rating=rating, description=description, images=img)
     return item_data
 
 async def scrape_noon_price(url: str) -> dict:
-    start_time = time.time()
+    headers = Headers(headers=True).generate()
     asession = AsyncHTMLSession()
-    response = await asession.get(url)
+    try:
+     response = await asession.get(url, headers=headers,timeout=10)
+    except:
+        return {"error": "timeout"}
     soup = BeautifulSoup(response.content, 'html.parser')
 
     price = soup.find('div', class_='priceNow').get_text().split('.')[0].strip()
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
 
     item_data = ItemPrice(price=price)
     return item_data
 
 async def scrape_noon_images(url: str) -> dict:
+    headers = Headers(headers=True).generate()
     asession = AsyncHTMLSession()
-    response = await asession.get(url)
+    try:
+     response = await asession.get(url, headers=headers,timeout=10)
+    except:
+        return {"error": "timeout"}   
     soup = BeautifulSoup(response.content, 'html.parser')
     result= soup.select('img')
     img=[]
