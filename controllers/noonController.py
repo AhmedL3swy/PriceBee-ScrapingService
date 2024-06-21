9f50ec0b6a4b588336f2a85ebce4f04a25bafbc8
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-06-22 00:35:59 +0300

    update noon

--- controllers/noonController.py
+++ controllers/noonController.py
@@ -3,17 +3,16 @@ from requests_html import AsyncHTMLSession
 from models.Product import ProductDetailDTO
 from fake_headers import Headers
 
+
 async def scrape_noon_full(url: str) -> dict:
-    headers = Headers(headers=True).generate()
     asession = AsyncHTMLSession()
     try:
-     response = await asession.get(url, headers=headers,timeout=10)
+     response = await asession.get(url,timeout=10)
     except:
-        return {"error": "timeout"}
-    
+        raise Exception("timeout")
     soup = BeautifulSoup(response.content, 'html.parser')
     title = soup.find('h1').get_text().strip()
-    price = soup.find('div', class_='priceNow').get_text().split('.')[0].strip()
+    price = float(soup.find('div', class_='priceNow').get_text().split('.')[0].strip())
     rating = soup.find('div', class_='sc-e7071e85-2').get_text().strip()
     description = soup.find('div', class_='sc-97eb4126-2').get_text().strip()
     image_soup = soup.find_all('img')
