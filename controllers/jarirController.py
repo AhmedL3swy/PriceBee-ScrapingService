5d0c66e02c18c7236ef230e25024fa161834f771
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-06-21 23:33:05 +0300

    jarir Tested

--- controllers/jarirController.py
+++ controllers/jarirController.py
@@ -1,22 +1,27 @@
 #Dependencies
 from requests_html import AsyncHTMLSession
-from utils import getStringBetweenTwoWords
+from utils import getStringBetweenTwoWords , extract_and_stringify_object
 from models.Product import ProductDetailDTO
+import json
 
 async def scrape_Jarir_full(url):  
     session = AsyncHTMLSession()
     response = await session.get(url)
-    script_target_object=getStringBetweenTwoWords(response.content.decode(), 'window.__INITIAL_STATE__= ','</script>')
-    strat_index=response.content.decode().index('window.__INITIAL_STATE__')
-    end_index=response.content.decode().index('</script>',strat_index)
-    script_target_object=response.content.decode()[strat_index:end_index].replace('window.__INITIAL_STATE__=','')
-    price=getStringBetweenTwoWords(script_target_object, 'final_price_ex_tax:',',')
-    Title=getStringBetweenTwoWords(script_target_object, 'GTM_name:',',')
-    images_string = getStringBetweenTwoWords(script_target_object, 'media_gallery','tsk').split("},{")
-    images=[]
-    for i in range(len(images_string)):
-        images.append("https://ak-asset.jarir.com/akeneo-prod/asset/"+getStringBetweenTwoWords(images_string[i], 'image:"','",lab'))
-    item_data = ProductDetailDTO(name_Global=Title, price=price,images=images,productlink1=url)
+    soup=response.content.decode()
+    stringObj = getStringBetweenTwoWords(soup, 'original:', ',related').replace('!1', 'true').replace('!0', 'false')
+    obj = json.loads(extract_and_stringify_object(stringObj))
+
+    specs = getStringBetweenTwoWords(str(obj), 'homedelivery_enable', 'check_availability_status')
+    specs_list = [spec.strip() for spec in specs.split(',') if ":" in spec]
+    concatenated_specs = ', '.join(specs_list)
+
+    name_global = obj.get("name", "")
+    isAvailable = obj.get("check_availability_status", 0) == 1
+    PriceAfterDiscount = obj.get("final_price_ex_tax", obj.get("price", None))
+    rating = None  
+    
+    images = ["https://ak-asset.jarir.com/akeneo-prod/asset/"+image["image"] for image in obj.get("media_gallery", []) if image.get("type") == "image"]
+    item_data = ProductDetailDTO(name_Global=name_global, price=PriceAfterDiscount,images=images,productlink1=url,description_Global=concatenated_specs,is_available=isAvailable,rating=rating)
     return item_data
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #utilts
 import os
 import json
+import json5
 def printSoupToHtml(soup):
     #Creating a file if not exist
     if not os.path.exists('soup.html'):
@@ -26,3 +27,24 @@ def getStringBetweenTwoWords(string, first, last):
     except ValueError:
         return ""
 
+def extract_and_stringify_object(original):
+    for i in range(0, len(original)):
+        if original[i] == '{':
+            start = i
+            break
+    for i in range(len(original) - 1, -1, -1):
+        if original[i] == '}':
+            end = i
+            break  
+    if end < len(original) - 1 and original[end + 1] != ',':
+        original = original[:end + 1] + ',' + original[end + 1:]
+    return json.dumps(json5.loads(original[start:end + 1]), indent=4)
+
+def printJsonToFile(data, filename):
+    #Creating a file if not exist
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write('')
+    with open (filename, 'w', encoding='utf-8') as file:
+        file.write(json.dumps(data, indent=4))
+    return True
